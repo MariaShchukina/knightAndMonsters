132687fa4d1eac4f73f3b97255750aeeacdd4a36
MiaShev <mshukina2005@gmail.com>
2022-05-08 13:10:03 +0300

    meeting with monster

--- src/main/kotlin/MainGame.kt
+++ src/main/kotlin/MainGame.kt
@@ -1,13 +1,16 @@
 
-var HEALTH = 0
-var POWER = 0
+var HEALTH = 1
+var POWER = 1
 var COUNTOFKILLEDMONSTERS = 0
 fun main() {
     welcome()
     healthOfKnight()
     powerOfKnight()
-    println("Ваше здоровье : $HEALTH")
-    println("Ваше здоровье : $POWER")
+    while (COUNTOFKILLEDMONSTERS < 10) {
+        println("Ваше здоровье : $HEALTH")
+        println("Ваша сила : $POWER")
+        meetingWithMonster()
+    }
 }
 
 fun welcome(){
@@ -24,3 +27,47 @@ fun powerOfKnight(){
     POWER = pp
 }
 
+fun meetingWithMonster() {
+    var monsterPower = createHealthOfMonster()
+    var monsterHealth = createPowerOfMonster()
+    userChoice(monsterPower, monsterHealth)
+}
+
+fun createHealthOfMonster():Int {
+    var hpOfMonster = (10..20).random()
+    println("Здоровье монстра: $hpOfMonster")
+    return hpOfMonster
+}
+
+fun createPowerOfMonster():Int {
+    var ppOfMonster = (10..20).random()
+    println("Сила монстра: $ppOfMonster")
+    return ppOfMonster
+}
+
+fun userChoice(monsterPower: Int, monsterHealth: Int) {
+    println("Вы можете атаковать монста (нажмите 1) или убежать (нажмите 2)")
+    val knightInput = readLine()
+    var monsterHP = monsterHealth
+    var monsterPP = monsterPower
+    if (knightInput != null) {
+        if (knightInput.toInt() == 1) {
+            while (HEALTH > 0 && monsterHP > 0){
+                HEALTH -= monsterPower
+                if (HEALTH > 0) {
+                    monsterHP -= POWER
+                    if (monsterHP <= 0) {
+                        COUNTOFKILLEDMONSTERS += 1
+                        println("Молодец, ты победил! Ваше здоровье: $HEALTH. Убитых монстров: $COUNTOFKILLEDMONSTERS.")
+                    }
+                } else {
+                    println("вы умерли")
+                }
+            }
+        } else if (knightInput.toInt() == 2) {
+            println("Вы убежали. Ваше здоровье: $HEALTH. Убитых монстров: $COUNTOFKILLEDMONSTERS.")
+        } else {
+            println("Попробуй ещё раз")
+        }
+    }
+}
